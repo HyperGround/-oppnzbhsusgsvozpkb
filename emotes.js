69a30fb600c9a2e52d4b7be57bea1d1b162b33a2
HyperGround <112865572+HyperGround@users.noreply.github.com>
2024-02-03 22:07:10 +0300

    Delete emotes.js

--- emotes.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  MESSAGE: {
-    y: "<:tick_yes:1149274257646428170> ", // yes 
-    x: "<:tick_no:1149274264701255762>", // no
-    i: "<:info:1149697984620138506> ", // info
-    l: "<a:Search:1129598461893935184>" // loading
-  }
-}
