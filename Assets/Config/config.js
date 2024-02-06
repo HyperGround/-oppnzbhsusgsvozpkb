4986176b4c9f2bc2a0e8dee16a45215d89d220c0
HyperGround <112865572+HyperGround@users.noreply.github.com>
2024-02-06 13:27:39 +0300

    Update config.js

--- Assets/Config/config.js
+++ Assets/Config/config.js
@@ -1,9 +1,9 @@
 module.exports = {
   TOKEN: process.env.TOKEN || "",
-  MONGO_DB: process.env.MONGO_DB || "",
-  CLIENT_ID: process.env.CLIENT_ID || "",
+  MONGO_DB: process.env.MONGO_DB || "mongodb+srv://Jarvis:nitrixexe@cluster0.bxzzf70.mongodb.net/?retryWrites=true&w=majority",
+  CLIENT_ID: process.env.CLIENT_ID || "1201112369892425789",
   OWNERS: ["999661452010995832"],
-  SUPPORT_SERVER: "https://discord.com/invite/pCRGEJKjUs",
+  SUPPORT_SERVER: "https://discord.com/invite/rYHH2Tjz5e",
   CHANNELS: {
     COMMANDS_LOGS: "1196565653629448342",
     ERROR_COMMAND_LOGS: "1191725581465825400"
